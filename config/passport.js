a95943960ac6137d45c6b915f73f12a3b28cfc8d
chyon8 <chyon8@hotmail.com>
2024-01-18 13:15:19 +0900

    new

--- config/passport.js
+++ config/passport.js
@@ -8,8 +8,8 @@ module.exports = function (passport) {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        //callbackURL: '/auth/google/callback',
-        callbackURL: '/stories',
+      callbackURL: '/auth/google/callback',
+       // callbackURL: '/stories',
         
       },
       async (accessToken, refreshToken, profile, done) => {
