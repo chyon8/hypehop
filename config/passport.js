27450bea637eb97d5e31250b70110e6979045637
chyon8 <chyon8@hotmail.com>
2024-01-28 15:07:27 +0900

    googlefix3

--- config/passport.js
+++ config/passport.js
@@ -18,8 +18,8 @@ module.exports = function (passport) {
         const newUser = {
           googleId: profile.id,
           displayName: profile.displayName,
-          firstName: profile.name?.givenName ?? "No name" ,
-          lastName: profile.name?.familyName ?? "No name",
+          firstName: profile.name?.givenName ,
+          lastName: profile.name?.familyName,
           image: profile.photos[0].value,
         }
 
